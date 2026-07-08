16b2700fb85b84bc73ad93f26bb8448746542a1b
Sahil Khatik <sahilkhatik068@gmail.com>
2026-07-09 00:22:42 +0530

    Setup Express server and MongoDB connection

---
+++ backend/server.js
@@ -0,0 +1,21 @@
+const express = require('express');
+const cors = require('cors');
+const connectDB = require('./config/db');
+const PORT = process.env.PORT || 5000
+
+require("dotenv").config();
+
+connectDB() //You call the database connection here 
+
+const app = express();
+
+app.use(cors()); //Allowed frontend to communicate with backend
+app.use(express.json()) //Allows Express to receive JSON data from your React frontend.
+
+app.get("/", (req, res) => {
+    res.send("Job Tracker Backend Running...")
+})
+
+app.listen(PORT, () => {
+    console.log(`🚀 Server running on port ${PORT}`);
+})
