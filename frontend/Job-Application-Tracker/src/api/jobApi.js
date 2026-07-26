d8c7d64bfcb32c045ceca51309bed8f0a142d4ca
Sahil Khatik <sahilkhatik068@gmail.com>
2026-07-26 11:41:38 +0530

    Secure API in .env file

--- frontend/Job-Application-Tracker/src/api/jobApi.js
+++ frontend/Job-Application-Tracker/src/api/jobApi.js
@@ -1,4 +1,4 @@
-const BASE_URL = "https://job-tracker-backend-b873.onrender.com/api/jobs";
+const BASE_URL = import.meta.env.VITE_API_URL;
 
 export async function createJob(jobData) {
     const response = await fetch(BASE_URL, {
